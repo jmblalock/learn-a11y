d7eff18947972e8301be3ac22d52a7b021c03da9
jmblalock <justin.m.blalock@gmail.com>
2021-02-23 11:27:40 -0600

    screen-readers

--- js/modal.js
+++ js/modal.js
@@ -13,7 +13,8 @@ function open() {
   const previouslyFocused = document.activeElement;
 
   var focusableElements = modal.querySelectorAll('a[href], area[href], input:not([disabled]):not([type="hidden"]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable], audio[controls], video[controls], summary, [tabindex^="0"], [tabindex^="1"], [tabindex^="2"], [tabindex^="3"], [tabindex^="4"], [tabindex^="5"], [tabindex^="6"], [tabindex^="7"], [tabindex^="8"], [tabindex^="9"]');
-  focusableElements = Array.prototype.slice.call(focusableElements);
+  // focusableElements = Array.prototype.slice.call(focusableElements);
+  focusableElements = Array.from(focusableElements);
 
   const firstItem = focusableElements[0];
   const lastItem = focusableElements[focusableElements.length - 1];
@@ -41,6 +42,8 @@ function open() {
           firstItem.focus();
         }
       }
+    } else if (e.keyCode === 27) {
+      close();
     }
   }
 }
